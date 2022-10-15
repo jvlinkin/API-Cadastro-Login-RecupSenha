52bb891b9792489ac4614699c8b299442a32708d
JoaoPaiva <jvlimapaiva@gmail.com>
2022-10-15 15:41:03 -0300

    fix: configs

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,9 @@
-import { application, Router } from "express";
+import { Router } from "express";
 import usersRouter from "../modules/users/UserRoutes";
 
 const routes = Router();
 
 //Rotas aqui
-routes.use('/user',usersRouter);
+routes.use("/user", usersRouter);
 
 export default routes;
